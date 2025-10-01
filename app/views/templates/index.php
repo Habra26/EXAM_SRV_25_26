870bf33834a440e8a23592dd409e45adc9da4ffb
Habra26 <arnaud.habraken@outlook.com>
2025-10-01 14:53:03 +0200

    Intégration template et découpage en partials

---
+++ app/views/templates/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <?php include_once '../app/views/templates/partials/_head.php'; ?>
+  </head>
+
+  <body>
+    <!-- Preloader Start -->
+    <div class="preloader">
+      <div class="rounder"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <div id="main">
+      <div class="container">
+        <div class="row">
+            
+          <?php include_once '../app/views/templates/partials/_aside.php'; ?>
+
+          <?php include_once '../app/views/templates/partials/_main.php'; ?>
+
+          <?php include_once '../app/views/templates/partials/_footer.php'; ?>
+
+          </div>
+          <!-- Blog Post (Right Sidebar) End -->
+        </div>
+      </div>
+    </div>
+
+    <!-- Back to Top Start -->
+    <a href="#" class="scroll-to-top"><i class="fa fa-long-arrow-up"></i></a>
+    <!-- Back to Top End -->
+
+    <!-- All Javascript Plugins  -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/plugin.js"></script>
+
+    <!-- Main Javascript File  -->
+    <script type="text/javascript" src="js/scripts.js"></script>
+  </body>
+</html>
